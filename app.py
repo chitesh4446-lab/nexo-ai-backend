ecc6d5455949ba0603f887a81f3aa0f002abdbd5
chitesh4446-lab <chitesh4446@gmail.com>
2026-03-02 10:37:30 +0530

    Update app.py

--- app.py
+++ app.py
@@ -18,3 +18,20 @@ async def remove_background(file: UploadFile = File(...)):
         io.BytesIO(output_bytes),
         media_type="image/png"
     )
+from fastapi import FastAPI, File, UploadFile
+from fastapi.responses import StreamingResponse
+from rembg import remove
+from PIL import Image
+import io
+
+app = FastAPI()
+
+@app.get("/")
+def home():
+    return {"message": "NEXO AI Backend Running"}
+
+@app.post("/remove-bg")
+async def remove_bg(file: UploadFile = File(...)):
+    input_bytes = await file.read()
+    output = remove(input_bytes)
+    return StreamingResponse(io.BytesIO(output), media_type="image/png")
